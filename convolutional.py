7518f6d24888241a4cb81c2ee34a7c2905e060e1
Tony R <tonydraf@gmail.com>
2015-12-07 13:06:29 +0100

    Second Convolutional Layer

--- convolutional.py
+++ convolutional.py
@@ -51,6 +51,12 @@ h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
 h_pool1 = max_pool_2x2(h_conv1)
 
 
+# "In order to build a deep network, we stack several layers of this type.
+# The second layer will have 64 features for each 5x5 patch."
+W_conv2 = weight_variable([5, 5, 32, 64])
+b_conv2 = bias_variable([64])
+h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
+h_pool2 = max_pool_2x2(h_conv2)
 
 
 sess.run(tf.initialize_all_variables())
