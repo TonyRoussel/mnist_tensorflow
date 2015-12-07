71657d650d075614de9be24c7e5185593c38c06e
Tony R <tonydraf@gmail.com>
2015-12-07 12:46:12 +0100

    Convolution and Pooling

--- convolutional.py
+++ convolutional.py
@@ -21,6 +21,18 @@ def bias_variable(shape):
     return tf.Variable(initial)
 
 
+# "TensorFlow also gives us a lot of flexibility in convolution and pooling operations.
+# How do we handle the boundaries? What is our stride size?
+# In this example, we're always going to choose the vanilla version.
+# Our convolutions uses a stride of one and are zero padded so that the output is the same size as the input.
+# Our pooling is plain old max pooling over 2x2 blocks. To keep our code cleaner,
+# let's also abstract those operations into functions."
+def conv2d(x, W):
+    return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
+
+def max_pool_2x2(x):
+    return tf.nn.max_pool(x, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
+
 # Data importation via google data import script.
 # mnist is a class which store training, validation and testing sets as numpy arrays.
 mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
